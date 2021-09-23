a94d860bfafd9ed3cd64b6847b7a813e2c781147
YevgeniyPivnyak <dneprojek@gmail.com>
2021-09-23 15:10:38 +0300

    dev pages add description

--- client/src/pages/PostEdit.tsx
+++ client/src/pages/PostEdit.tsx
@@ -1,3 +1,20 @@
+import { makeStyles } from "@material-ui/styles";
+import { Button } from "@material-ui/core";
+import { AppTheme } from "../config/theme";
+
+const useStyles = makeStyles<AppTheme>((theme) => ({
+  root: {
+    ...theme.page,
+  },
+}));
+
 export const PostEdit = () => {
-  return <h1>Post Edit</h1>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <h1>Post Edit</h1>
+      <Button color="primary">Contained</Button>
+    </div>
+  );
 };
